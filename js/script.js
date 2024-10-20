3e4001243b3b9db9bd33bf1f537fa4f1a99c81da
Rafaelrs21 <santireis14@gmail.com>
2024-10-20 19:51:01 -0300

    Formulario funcionando

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Pegue o elemento do hamburger menu
-var hamburger = document.querySelector('.hamburger-menu');
-var nav = document.querySelector('.main-nav');
-
-// Adicione um evento de clique ao hamburger para alternar a visibilidade do menu
-hamburger.addEventListener('click', function() {
-    nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
-});
