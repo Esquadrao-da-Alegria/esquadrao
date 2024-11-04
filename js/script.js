c2a1dfd79383352e6eeac90adc2d752bb3a55a9c
Rafaelrs21 <santireis14@gmail.com>
2024-11-04 17:48:49 -0300

    Feito as atualizacoes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-function showContent(city) {
-    // Oculta todos os conteúdos
+let activeItem = null;
+
+function showContent(city, element) {
+    // Se um item ativo já existir e for diferente do item clicado, reseta a seta e oculta o conteúdo
+    if (activeItem && activeItem !== element) {
+        activeItem.querySelector('.arrow').textContent = '▲';
+    }
+
+    // Exibe o conteúdo da cidade selecionada
     let contents = document.querySelectorAll('.content');
     contents.forEach(function(content) {
         content.style.display = 'none';
     });
-
-    // Exibe o conteúdo da cidade selecionada
     document.getElementById(city).style.display = 'block';
+
+    // Define o novo item ativo, altera a seta para ▼ e mantém a seta assim
+    element.querySelector('.arrow').innerHTML = '▼';
+
+    // Atualiza o item ativo
+    activeItem = element;
 }
