b39e677ee63bd11fbae8d2d401b702053cbf51b0
Rafaelrs21 <santireis14@gmail.com>
2024-11-22 16:55:11 -0300

    Alterado o formulario

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function enviarFormulario() {
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const accessKey = document.getElementById('accessKey').value;
+
+    
+    const dados = {
+        name: nome,
+        email: email,
+        message: mensagem,
+        accessKey: accessKey,
+    };
+
+    fetch('https://api.staticforms.xyz/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dados),
+    })
+    .then((response) => {
+        if (response.ok) {
+            alert('Mensagem enviada com sucesso!');
+            document.getElementById('meuFormulario').reset(); 
+            setTimeout(() => location.reload(), 500);
+        } else {
+            throw new Error('Erro ao enviar a mensagem.');
+        }
+    })
+    .catch((error) => {
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao enviar a mensagem. Tente novamente mais tarde.');
+    });
+}
