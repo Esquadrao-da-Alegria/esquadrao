6cc26ff6bb2248c4f7e13855ca1a0575084f9c49
Arthur <arthurbufon2052@gmail.com>
2025-10-06 21:04:53 -0300

    CIDADES+ESTADOS: gerados models + seeders + migrations

--- projeto-laravel/database/seeders/DatabaseSeeder.php
+++ projeto-laravel/database/seeders/DatabaseSeeder.php
@@ -2,10 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,14 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
+        $this->call(
             [
-                'name' => 'Test User',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
+                EstadoSeeder::class,
+                CidadeSeeder::class,
             ]
         );
     }
