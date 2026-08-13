5f8d353690692603f8f2d873762ab5792619b79b
Bernardo Prates <barbed29@gmail.com>
2026-08-13 10:29:14 -0300

    feat: add optional free-text units visited field to visit reports

--- resources/js/types/relatorio.ts
+++ resources/js/types/relatorio.ts
@@ -3,6 +3,7 @@ import type { TipoRelatorio } from '@/types/visita'
 export type DadosFormulario = {
     tipo_relatorio: TipoRelatorio | ''
     ala_unidade_id: number | '' | null
+    unidades_visitadas: string
     resumo: string
     feedback: string
     quartos_visitados: number | ''
